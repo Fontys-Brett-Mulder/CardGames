106718133fa27dbc717742fdeca9e3d32d0291c3
492563@student.fontys.nl <Vribherfee5>
2022-09-12 17:15:59 +0200

    Added frontend vite vue project

---
+++ Code/vite-vue-frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <router-view />
+
+</template>
+
+<script setup>
+import CounterButton from '@/components/CounterButton.vue'
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<style>
+@import '@/styles/variables.css';
+
+*{
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  height: 100vh;
+  width: 100vw;
+  font-family:Arial, Helvetica, sans-serif
+}
+
+#app, .page{
+  height: 100%;
+  width: 100%;
+}
+
+</style>
---
+++ Code/vite-vue-frontend/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="homepage" class="page">
+        <h1>Card Games</h1>
+        <CounterButton />
+    </div>
+</template>
+
+<script setup>
+import CounterButton from '@/components/CounterButton.vue'
+
+</script>
+
+<style>
+body {
+    background-color: var(--color-hub-background);
+}
+
+#homepage {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
